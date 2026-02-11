2519ce950d7159698ab809f6825a8d70eec081ca
anqwampee <anastasiia.pudova@gmail.com>
2026-02-11 17:00:16 +0200

    Add pellets spawn

--- src/map/testMap.py
+++ src/map/testMap.py
@@ -1,6 +1,6 @@
 import pygame
 from src.utils.constants import TILE_SIZE, BLUE
-
+from src.game_objects.pellet import Pellet
 class Map():
     def __init__(self):
         self.walls = []
@@ -33,6 +33,8 @@ class Map():
         ]
 
         self.create_walls()
+        self.pellets = []
+        self.create_pellets()
     
     def create_walls(self):
         self.walls = []
@@ -42,6 +44,15 @@ class Map():
                 if tile == 1:
                     self.walls.append(pygame.Rect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE))
 
+    def create_pellets(self):
+        for y, row in enumerate(self.level):
+            for x, tile in enumerate(row):
+                if tile == 0:
+                    self.pellets.append(Pellet(x, y))
+
     def draw_map(self, screen):
         for wall in self.walls:
             pygame.draw.rect(screen, BLUE, wall, 2)
+        for pellet in self.pellets:
+            pellet.draw(screen)
+
