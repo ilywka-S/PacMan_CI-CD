d20067257d429b3cfa1609b053f51d992df6acda
Artemko <productionkrey@gmail.com>
2026-02-17 02:46:12 +0200

    Implement sfx for ghost movement

--- src/audio/sound_manager.py
+++ src/audio/sound_manager.py
@@ -10,6 +10,7 @@ class SoundManager:
             self.sounds['pacman_death'] = pygame.mixer.Sound('src/assets/sounds/pacman_death.mp3')
             self.sounds['pacman_win'] = pygame.mixer.Sound('src/assets/sounds/pacman_win.mp3')
             self.sounds['pacman_eat_dots'] = pygame.mixer.Sound('src/assets/sounds/pacman_eat_dots.mp3')
+            self.sounds['ghosts_normal_move'] = pygame.mixer.Sound('src/assets/sounds/ghosts_normal_move.mp3')
             self.sounds['pacman_eat_fruit'] = pygame.mixer.Sound('src/assets/sounds/pacman_eat_fruit.mp3')
         except FileNotFoundError as e:
             print(f"Error loading sound: {e}")
@@ -17,3 +18,13 @@ class SoundManager:
     def play_sound(self, sound_name):
         if sound_name in self.sounds:
             self.sounds[sound_name].play()
+
+    def stop_sound(self, sound_name):
+        if sound_name in self.sounds:
+            self.sounds[sound_name].stop()
+    
+    def play_sound_if_idle(self, sound_name):
+        if sound_name in self.sounds:
+            sound = self.sounds[sound_name]
+            if sound.get_num_channels() == 0:
+                sound.play()
